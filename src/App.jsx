f7b847b9e122fd7b28b3ab087df660efbc13089f
mouad <elbawmoad@gmail.com>
2025-08-07 17:14:34 -1100

    Implement the About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
 
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
             <Hero/>
-
-
-
+            <About/>
         </main>
     );
 };
